60509baed8a908cf9978b8fde703bd5d34371dce
Javier Gonzalez <javgon2306@gmail.com>
2024-09-02 10:47:28 -0300

    changes cron-services

--- src/presentation/cron/cron-service.ts
+++ src/presentation/cron/cron-service.ts
@@ -5,11 +5,8 @@ type OnTick = () => void;
 export class CronService {
 
     static createJob( cronTime: CronTime, onTick: OnTick): CronJob {
-
         const job = new CronJob(cronTime, onTick);
-        
         job.start();
-
         return job;
     }
 
